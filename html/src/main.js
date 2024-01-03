46a99af418daae41b33630053194a1ceb819f3d0
ruiji_git <1420767140@qq.com>
2024-01-03 13:33:26 +0800

    Site updated: 2024-01-03 13:33:26

---
+++ html/src/main.js
@@ -0,0 +1,5 @@
+import { Application } from '@splinetool/runtime';
+
+const canvas = document.getElementById('canvas3d');
+const app = new Application(canvas);
+app.load('https://prod.spline.design/BwWgDeyOB9x1oVLx/scene.splinecode');
