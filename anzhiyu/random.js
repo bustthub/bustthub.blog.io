28ffe6c06cfa2c5204f226d94154226f7efce806
ruiji_git <1420767140@qq.com>
2024-01-04 14:42:47 +0800

    Site updated: 2024-01-04 14:42:46

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2023/11/13/Unscreen/","2024/01/03/Suno ai/","2023/11/10/bing AI/","2023/11/11/collov.ai/","2023/11/13/dora/","2023/11/09/damo/","2023/11/13/lingma/","2023/11/13/meitu/","2023/11/10/meshy/","2023/11/11/midjourney/","2023/11/12/xinghuo/"];function toRandomPost(){
+var posts=["2024/01/03/Suno ai/","2023/11/10/bing AI/","2023/11/11/collovAI/","2023/11/13/Unscreen/","2023/11/09/damo/","2023/11/13/dora/","2023/11/13/lingma/","2023/11/13/meitu/","2023/11/10/meshy/","2023/11/11/midjourney/","2023/11/12/xinghuo/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
